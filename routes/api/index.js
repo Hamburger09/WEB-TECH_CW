e74a143e2b9268e4386e6ec50d09719903445041
Hamburger09 <akhmedov.20092004@gmail.com>
2025-04-05 17:01:32 +0500

    finished the index page

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const bookRouter = require("./book/index.js");
 
-router.use("/api", bookRouter);
+router.use("/api/book", bookRouter);
 
 module.exports = router;
